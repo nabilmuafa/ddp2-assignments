c43ddb5019d47942b1d9221e5ba6a81584813cc8
nabilmuafa <nmuafa@gmail.com>
2023-02-20 22:09:17 +0700

    feat: implemented main() and generateId()

--- assignment1/src/main/java/assignments/assignment1/NotaGenerator.java
+++ assignment1/src/main/java/assignments/assignment1/NotaGenerator.java
@@ -1,15 +1,47 @@
 package assignments.assignment1;
 
 import java.util.Scanner;
+import java.lang.Character;
 
 public class NotaGenerator {
     private static final Scanner input = new Scanner(System.in);
 
+    public static boolean isNumeric(String str){
+        char[] chars = str.toCharArray();
+        for (int i=0; i<chars.length; i++){
+            if (!Character.isDigit(chars[i])){
+                return false;
+            }
+        }
+        return true;
+    }
     /**
      * Method main, program utama kalian berjalan disini.
      */
     public static void main(String[] args) {
-        // TODO: Implement interface menu utama
+        String running = "1";
+        while (running != "0"){
+            printMenu();
+            System.out.print("Pilihan : ");
+            running = input.nextLine();
+            if (!running.equals("1") && !running.equals("2") && !running.equals("0")){
+                System.out.println("================================");
+                System.out.println("Pilihan tidak dikenali. Silahkan coba kembali.");
+            }
+            else if (running.equals("1")){
+                System.out.println("================================");
+                System.out.println("Masukkan nama anda:");
+                String nama = input.nextLine();
+                System.out.println("Masukkan nomor handphone anda:");
+                String nomorHP = input.nextLine();
+                while (!isNumeric(nomorHP)){
+                    System.out.println("Nomor HP hanya menerima digit");
+                    nomorHP = input.nextLine();
+                }
+                String idLaundry = generateId(nama, nomorHP);
+                System.out.println("ID Anda : " + idLaundry);   
+            }
+        }
     }
 
     /**
@@ -41,8 +73,26 @@ public class NotaGenerator {
      * @return String ID anggota dengan format [NAMADEPAN]-[nomorHP]-[2digitChecksum]
      */
     public static String generateId(String nama, String nomorHP){
-        // TODO: Implement generate ID sesuai soal.
-        return null;
+        String[] namaPerKata = nama.split(" ");
+        String namaKataPertama = namaPerKata[0].toUpperCase();
+        String idLaundry = namaKataPertama + "-" + nomorHP;
+        int checkSum = 0;
+        for (int i=0; i<idLaundry.length(); i++){
+            char karakterId = idLaundry.charAt(i);
+            if ((int)karakterId >= 48 && (int)karakterId < 58){
+                checkSum += Character.getNumericValue(karakterId);
+            }
+            else if ((int)karakterId >= 65 && (int)karakterId < 91){
+                checkSum += ((int)karakterId - 64);
+            }
+            else{
+                checkSum += 7;
+            }
+        }
+        String checkSumString = String.format("%02d", checkSum);
+        idLaundry += ("-" + checkSumString);
+        
+        return idLaundry;
     }
 
     /**
