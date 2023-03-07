a6bb3f9734d9a267fda2119bd7b68975aa03847a
nabilmuafa <nmuafa@gmail.com>
2023-03-07 13:48:22 +0700

    feat: implement Member class

--- assignment2/src/main/java/assignments/assignment2/Member.java
+++ assignment2/src/main/java/assignments/assignment2/Member.java
@@ -3,10 +3,32 @@ package assignments.assignment2;
 import assignments.assignment1.NotaGenerator;
 
 public class Member {
-    // TODO: tambahkan attributes yang diperlukan untuk class ini
+    private String nama;
+    private String noHp;
+    private String id;
+    private int bonusCounter;
     public Member(String nama, String noHp) {
-        // TODO: buat constructor untuk class ini
+        this.nama = nama;
+        this.noHp = noHp;
+        this.id = NotaGenerator.generateId(this.nama, this.noHp);
+        this.bonusCounter = 0;
     }
-
-    // TODO: tambahkan methods yang diperlukan untuk class ini
+    public String getNama(){
+        return this.nama;
+    }
+    public String getNoHp(){
+        return this.noHp;
+    }
+    public String getId(){
+        return this.noHp;
+    }
+    public int getBonusCounter(){
+        return this.bonusCounter;
+    }
+    public void addBonusCounter(){
+        this.bonusCounter++;
+    }
+    public void resetBonusCounter(){
+        this.bonusCounter = 0;
+    }
 }
