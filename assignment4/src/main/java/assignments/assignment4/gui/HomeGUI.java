4b602be99a09a8e2de4f2d26d207cb26d7cf7bc6
nabilmuafa <nmuafa@gmail.com>
2023-05-16 06:22:21 +0700

    feat: redesign HomeGUI

--- assignment4/src/main/java/assignments/assignment4/gui/HomeGUI.java
+++ assignment4/src/main/java/assignments/assignment4/gui/HomeGUI.java
@@ -38,28 +38,28 @@ public class HomeGUI extends JPanel {
      * Be creative and have fun!
      * */
     private void initGUI() {
-        titleLabel = new JLabel("Selamat datang di Cuci-Cuci System!");
-        dateLabel = new JLabel("Sekarang tanggal gatau");
+        titleLabel = new JLabel("Selamat datang di CuciCuci System!", SwingConstants.CENTER);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        add(titleLabel, BorderLayout.NORTH);
+
+        dateLabel = new JLabel("Sekarang tanggal ???", SwingConstants.CENTER);
+        add(dateLabel, BorderLayout.SOUTH);
+
+        c.gridx = 0;
+        c.gridy = GridBagConstraints.RELATIVE;
+        c.fill = GridBagConstraints.NONE;
+        c.anchor = GridBagConstraints.CENTER;
+        c.weightx = 1.0;
+        c.weighty = 0.5;
+        c.insets = new Insets(5, 5, 5, 5);
+
         loginButton = new JButton("Login");
         registerButton = new JButton("Register");
         toNextDayButton = new JButton("Next Day");
 
-        c.gridy = 0;
-        c.gridx = 0;
-        c.weighty = 0.5;
-        mainPanel.add(titleLabel, c);
-
-        c.gridy = 1;
         mainPanel.add(loginButton, c);
-
-        c.gridy = 2;
         mainPanel.add(registerButton, c);
-
-        c.gridy = 3;
         mainPanel.add(toNextDayButton, c);
-
-        c.gridy = 4;
-        mainPanel.add(dateLabel, c);
 
         registerButton.addActionListener(new ActionListener() {
             @Override
